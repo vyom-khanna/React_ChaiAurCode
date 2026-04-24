a46cdbb3179abac63f89d15821f28cca8c1029eb
vyom-khanna <vyomkhanna23@gmail.com>
2026-04-25 00:24:43 +0530

    got the idea of react fibre changes in the latest commit

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,18 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
   const addvalue = ()=>{
-    if(counter<20) setcounter(counter+1)
+    if(counter<20){
+      Hiteshcounter(prevcounter => prevcounter+1)
+      Hiteshcounter(prevcounter => prevcounter+1)
+      Hiteshcounter(prevcounter => prevcounter+1)
+      Hiteshcounter(prevcounter => prevcounter+1)
+    }
   }
   const removeValue = ()=>{
-    if(counter>0) setcounter(counter-1)
+    if(counter>0) Hiteshcounter(counter-1)
   }
 
-  let [counter,setcounter] = useState(15);
+  let [counter,Hiteshcounter] = useState(10) //input counter value in useState
 
   return (
     <>
